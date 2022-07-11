e75cd38f887ba8e505aaa3b552d5e21fadc99814
Sampada Dubey <freelance.sampada.dubey@gmail.com>
2022-07-12 02:48:33 +0530

    GH-5: Exercism-Java: LanguageList

---
+++ exercism/src/main/java/io/sampadadubey/java/exercism/LanguageList.java
@@ -0,0 +1,48 @@
+package io.sampadadubey.java.exercism;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class LanguageList {
+
+    private final List<String> languages = new ArrayList<>();
+
+    private final Set<String> excitingLanguages = new HashSet<String>() {
+        {
+            add("Java");
+            add("Kotlin");
+        }
+    };
+
+    public boolean isEmpty() {
+        return languages.isEmpty();
+    }
+
+    public void addLanguage(final String language) {
+        if (!containsLanguage(language)) {
+            languages.add(language);
+        }
+    }
+
+    public void removeLanguage(final String language) {
+        languages.remove(language);
+    }
+
+    public String firstLanguage() {
+        return !isEmpty() ? languages.get(0) : null;
+    }
+
+    public int count() {
+        return languages.size();
+    }
+
+    public boolean containsLanguage(final String language) {
+        return languages.contains(language);
+    }
+
+    public boolean isExciting() {
+        return languages.stream().anyMatch(excitingLanguages::contains);
+    }
+}

dc26304b1d4c393a4c5cbd4cff0c7adb505f47a4
Sampada Dubey <freelance.sampada.dubey@gmail.com>
2022-07-16 02:10:27 +0530

    GH-5: Exercism-Java: ResistorColor

---
+++ exercism/src/main/java/io/sampadadubey/java/exercism/ResistorColor.java
@@ -0,0 +1,12 @@
+package io.sampadadubey.java.exercism;
+
+public class ResistorColor {
+
+    int colorCode(final String color) {
+        return ResistorColorCode.getColorCodeByName(color).getColorCode();
+    }
+
+    String[] colors() {
+        return ResistorColorCode.colors();
+    }
+}
---
+++ exercism/src/test/java/io/sampadadubey/java/exercism/ResistorColorTest.java
@@ -0,0 +1,37 @@
+package io.sampadadubey.java.exercism;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ResistorColorTest {
+
+    private ResistorColor resistorColor;
+
+    @BeforeEach
+    void setup() {
+        resistorColor = new ResistorColor();
+    }
+
+    @Test
+    void testBlackColorCode() {
+        assertThat(resistorColor.colorCode("black")).isEqualTo(0);
+    }
+
+    @Test
+    void testWhiteColorCode() {
+        assertThat(resistorColor.colorCode("white")).isEqualTo(9);
+    }
+
+    @Test
+    void testOrangeColorCode() {
+        assertThat(resistorColor.colorCode("orange")).isEqualTo(3);
+    }
+
+    @Test
+    void testColors() {
+        assertThat(resistorColor.colors()).containsExactly(
+                "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white");
+    }
+}

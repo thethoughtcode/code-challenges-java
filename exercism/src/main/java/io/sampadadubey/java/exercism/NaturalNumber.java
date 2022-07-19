58017e5cf00c753221f0e6975b77f02ced57d479
Sampada Dubey <freelance.sampada.dubey@gmail.com>
2022-07-20 00:31:29 +0530

    GH-5: Exercism-Java: Perfect Numbers

---
+++ exercism/src/main/java/io/sampadadubey/java/exercism/NaturalNumber.java
@@ -0,0 +1,61 @@
+package io.sampadadubey.java.exercism;
+
+public class NaturalNumber {
+
+    private final int number;
+
+    public NaturalNumber(final int number) {
+
+        if (number <= 0) {
+            throw new IllegalArgumentException("You must supply a natural number (positive integer)");
+        }
+
+        this.number = number;
+    }
+
+    public Classification getClassification() {
+
+        final Classification result;
+
+        if (number != 1) {
+
+            int loopNumber = number;
+            int sum = 1;
+            int divisor = 2;
+
+            while (divisor < loopNumber) {
+
+                final double dividendWithDecimal = ((double) number) / divisor;
+                final double dividendWithoutDecimal = Math.floor(dividendWithDecimal);
+
+                if (dividendWithDecimal == dividendWithoutDecimal) {
+                    loopNumber = (int) dividendWithDecimal;
+                    if (divisor != loopNumber) {
+                        sum = sum + divisor + loopNumber;
+                    } else {
+                        sum = sum + divisor;
+                    }
+                }
+
+                divisor++;
+            }
+
+            if (sum < number) {
+                result = Classification.DEFICIENT;
+            } else if (sum > number) {
+                result = Classification.ABUNDANT;
+            } else {
+                result = Classification.PERFECT;
+            }
+
+        } else {
+            result = Classification.DEFICIENT;
+        }
+
+        return result;
+    }
+}
+
+enum Classification {
+    DEFICIENT, PERFECT, ABUNDANT
+}

bfc548a87851a5e286337884ead3e22089b6cc31
Sampada Dubey <freelance.sampada.dubey@gmail.com>
2022-07-21 01:22:56 +0530

    GH-5: Exercism-Java: Elon's Toy Car

---
+++ exercism/src/main/java/io/sampadadubey/java/exercism/ElonsToyCar.java
@@ -0,0 +1,45 @@
+package io.sampadadubey.java.exercism;
+
+public class ElonsToyCar {
+
+    static final int DRIVE_DISTANCE = 20;
+
+    static final int BATTERY_USAGE = 1;
+
+    public static ElonsToyCar buy() {
+        return new ElonsToyCar();
+    }
+
+    private int distance;
+
+    private int battery;
+
+    ElonsToyCar() {
+        this.distance = 0;
+        this.battery = 100;
+    }
+
+    public String distanceDisplay() {
+        return String.format("Driven %s meters", distance);
+    }
+
+    public String batteryDisplay() {
+
+        final String display;
+
+        if (battery == 0) {
+            display = "Battery empty";
+        } else {
+            display = String.format("Battery at %s%%", battery);
+        }
+
+        return display;
+    }
+
+    public void drive() {
+        if (battery != 0) {
+            distance = distance + DRIVE_DISTANCE;
+            battery = battery - BATTERY_USAGE;
+        }
+    }
+}

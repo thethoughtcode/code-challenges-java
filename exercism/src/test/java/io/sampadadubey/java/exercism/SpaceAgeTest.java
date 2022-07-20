64dc5ce9254e47d8d38a1e342f00a0419fd6f99d
Sampada Dubey <freelance.sampada.dubey@gmail.com>
2022-07-21 02:52:25 +0530

    GH-5: Exercism-Java: Codacy violation fixes

--- exercism/src/test/java/io/sampadadubey/java/exercism/SpaceAgeTest.java
+++ exercism/src/test/java/io/sampadadubey/java/exercism/SpaceAgeTest.java
@@ -11,41 +11,41 @@ class SpaceAgeTest {
 
     @Test
     void ageOnEarth() {
-        assertThat(new SpaceAge(1000000000).onEarth()).isEqualTo(31.69, MAXIMUM_DELTA);
+        assertThat(new SpaceAge(1_000_000_000).onEarth()).isEqualTo(31.69, MAXIMUM_DELTA);
     }
 
     @Test
     void ageOnMercury() {
-        assertThat(new SpaceAge(2134835688).onMercury()).isEqualTo(280.88, MAXIMUM_DELTA);
+        assertThat(new SpaceAge(2_134_835_688).onMercury()).isEqualTo(280.88, MAXIMUM_DELTA);
     }
 
     @Test
     void ageOnVenus() {
-        assertThat(new SpaceAge(189839836).onVenus()).isEqualTo(9.78, MAXIMUM_DELTA);
+        assertThat(new SpaceAge(189_839_836).onVenus()).isEqualTo(9.78, MAXIMUM_DELTA);
     }
 
     @Test
     void ageOnMars() {
-        assertThat(new SpaceAge(2129871239L).onMars()).isEqualTo(35.88, MAXIMUM_DELTA);
+        assertThat(new SpaceAge(2_129_871_239).onMars()).isEqualTo(35.88, MAXIMUM_DELTA);
     }
 
     @Test
     void ageOnJupiter() {
-        assertThat(new SpaceAge(901876382).onJupiter()).isEqualTo(2.41, MAXIMUM_DELTA);
+        assertThat(new SpaceAge(901_876_382).onJupiter()).isEqualTo(2.41, MAXIMUM_DELTA);
     }
 
     @Test
     void ageOnSaturn() {
-        assertThat(new SpaceAge(2000000000L).onSaturn()).isEqualTo(2.15, MAXIMUM_DELTA);
+        assertThat(new SpaceAge(2_000_000_000).onSaturn()).isEqualTo(2.15, MAXIMUM_DELTA);
     }
 
     @Test
     void ageOnUranus() {
-        assertThat(new SpaceAge(1210123456L).onUranus()).isEqualTo(0.46, MAXIMUM_DELTA);
+        assertThat(new SpaceAge(1_210_123_456).onUranus()).isEqualTo(0.46, MAXIMUM_DELTA);
     }
 
     @Test
     void ageOnNeptune() {
-        assertThat(new SpaceAge(1821023456L).onNeptune()).isEqualTo(0.35, MAXIMUM_DELTA);
+        assertThat(new SpaceAge(1_821_023_456).onNeptune()).isEqualTo(0.35, MAXIMUM_DELTA);
     }
 }

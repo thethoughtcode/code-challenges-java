92f2fcbefe986c2d1c09477b0fece5820fea273c
Sampada Dubey <freelance.sampada.dubey@gmail.com>
2022-07-21 01:23:21 +0530

    GH-5: Exercism-Java: Salary Calculator

---
+++ exercism/src/main/java/io/sampadadubey/java/exercism/SalaryCalculator.java
@@ -0,0 +1,30 @@
+package io.sampadadubey.java.exercism;
+
+public class SalaryCalculator {
+
+    static final double BASE_SALARY = 1000.0;
+
+    static final double MAX_SALARY = 2000.0;
+
+    public double multiplierPerDaysSkipped(final int daysSkipped) {
+        return daysSkipped > 5 ? 0.85 : 1.0;
+    }
+
+    public int multiplierPerProductsSold(final int productsSold) {
+        return productsSold > 20 ? 13 : 10;
+    }
+
+    public double bonusForProductSold(final int productsSold) {
+        return multiplierPerProductsSold(productsSold) * productsSold;
+    }
+
+    public double finalSalary(final int daysSkipped, final int productsSold) {
+
+        final double daySalary = BASE_SALARY * multiplierPerDaysSkipped(daysSkipped);
+        final double bonus = bonusForProductSold(productsSold);
+
+        final double salary = daySalary + bonus;
+
+        return daySalary + bonus < MAX_SALARY ? salary : MAX_SALARY;
+    }
+}

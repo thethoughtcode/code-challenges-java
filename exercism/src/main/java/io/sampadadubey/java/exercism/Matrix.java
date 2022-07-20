95322f4444b6e840fc9295f6d06213062b97e7d0
Sampada Dubey <freelance.sampada.dubey@gmail.com>
2022-07-21 01:22:24 +0530

    GH-5: Exercism-Java: Matrix

---
+++ exercism/src/main/java/io/sampadadubey/java/exercism/Matrix.java
@@ -0,0 +1,60 @@
+package io.sampadadubey.java.exercism;
+
+class Matrix {
+
+    private final int rows;
+
+    private final int cols;
+
+    private final int[][] matrix;
+
+    Matrix(final String matrixAsString) {
+
+        final String[] rowData = matrixAsString.split("\\n");
+        final String[] firstColumnData = rowData[0].split(" ");
+
+        this.rows = rowData.length;
+        this.cols = rowData[0].split(" ").length;
+        this.matrix = new int[rowData.length][firstColumnData.length];
+
+        int row = 0;
+        String[] currentRowData;
+
+        do {
+
+            currentRowData = rowData[row].split(" ");
+
+            matrix[row] = new int[cols];
+
+            for (int col = 0; col < cols; col++) {
+                matrix[row][col] = Integer.parseInt(currentRowData[col]);
+            }
+
+            row++;
+
+        } while (row < rowData.length);
+
+    }
+
+    int[] getRow(final int rowNumber) {
+
+        final int[] rowData = new int[cols];
+
+        for (int col = 0; col < cols; col++) {
+            rowData[col] = matrix[rowNumber - 1][col];
+        }
+
+        return rowData;
+    }
+
+    int[] getColumn(final int columnNumber) {
+
+        final int[] colData = new int[rows];
+
+        for (int row = 0; row < rows; row++) {
+            colData[row] = matrix[row][columnNumber - 1];
+        }
+
+        return colData;
+    }
+}

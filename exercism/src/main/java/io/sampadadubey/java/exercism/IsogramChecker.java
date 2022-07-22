2553776d122efa0a2af8a7d2aad005d37cec1695
Sampada Dubey <freelance.sampada.dubey@gmail.com>
2022-07-22 21:57:44 +0530

    GH-5: Exercism-Java: Isogram Checker

---
+++ exercism/src/main/java/io/sampadadubey/java/exercism/IsogramChecker.java
@@ -0,0 +1,33 @@
+package io.sampadadubey.java.exercism;
+
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
+
+class IsogramChecker {
+
+    boolean isIsogram(final String phrase) {
+
+        final char[] characters = phrase.toLowerCase(Locale.getDefault()).toCharArray();
+
+        final Set<Character> alphabets = new HashSet<>(characters.length);
+
+        boolean isogram = true;
+
+        for (int index = 0; index < characters.length; index++) {
+
+            final char currentCharacter = characters[index];
+
+            if (currentCharacter == ' ' || currentCharacter == '-') {
+                continue;
+            } else if (alphabets.contains(currentCharacter)) {
+                isogram = false;
+                break;
+            } else {
+                alphabets.add(currentCharacter);
+            }
+        }
+
+        return isogram;
+    }
+}

626502e410243735aabd6d9cf7d8a60fe93f2ef5
Sampada Dubey <freelance.sampada.dubey@gmail.com>
2023-02-05 08:30:29 +0530

    GH-5: Exercism-Java: Football Match Reports

--- exercism/src/main/java/io/sampadadubey/java/exercism/FootballMatchReports.java
+++ exercism/src/main/java/io/sampadadubey/java/exercism/FootballMatchReports.java
@@ -4,36 +4,48 @@ import java.util.Optional;
 
 public class FootballMatchReports {
 
+    static final int GOALIE = 1;
+    static final int LEFT_BACK = 2;
+    static final int CENTER_BACK_ONE = 3;
+    static final int CENTER_BACK_TWO = 4;
+    static final int RIGHT_BACK = 5;
+    static final int LEFT_MIDFIELDER = 6;
+    static final int CENTER_MIDFIELDER = 7;
+    static final int RIGHT_MIDFILDER = 8;
+    static final int LEFT_WING = 9;
+    static final int STRIKER = 10;
+    static final int RIGHT_WING = 11;
+
     public static String onField(final int shirtNum) {
 
         String description = null;
 
         switch (shirtNum) {
-            case 1:
+            case GOALIE:
                 description = "goalie";
                 break;
-            case 2:
+            case LEFT_BACK:
                 description = "left back";
                 break;
-            case 3:
-            case 4:
+            case CENTER_BACK_ONE:
+            case CENTER_BACK_TWO:
                 description = "center back";
                 break;
-            case 5:
+            case RIGHT_BACK:
                 description = "right back";
                 break;
-            case 6:
-            case 7:
-            case 8:
+            case LEFT_MIDFIELDER:
+            case CENTER_MIDFIELDER:
+            case RIGHT_MIDFILDER:
                 description = "midfielder";
                 break;
-            case 9:
+            case LEFT_WING:
                 description = "left wing";
                 break;
-            case 10:
+            case STRIKER:
                 description = "striker";
                 break;
-            case 11:
+            case RIGHT_WING:
                 description = "right wing";
                 break;
             default:
